6c6fa62f6f14bf9b36e14a2e3d69ab58f5dd7f1d
hitthemoney <50669620+hitthemoney@users.noreply.github.com>
2020-08-26 15:47:25 -0700

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,142 @@
+zip.workerScriptsPath = "/lib/";
+
+var fileInput = document.getElementById("fileInput"),
+    input = document.getElementById("input"),
+    zipCList = document.getElementById("zipCList"),
+    pInput = document.getElementById("pInput"),
+    popups = ["changelog"];
+
+async function getVersion() {
+    var version = document.getElementById("version"),
+        changelog = await fetch("changelog.txt"),
+        changelogText = await changelog.text(),
+        changelog2 = document.getElementById("changelog");
+    version.innerHTML = changelogText.slice(0, 6);
+    changelog2.innerHTML = changelogText;
+}
+getVersion();
+
+function showChangelog() {
+    let changelogHolder = document.getElementById("changelogHolder");
+    changelogHolder.style.display = "block";
+    document.getElementById("popupHolder").style.display = "block";
+}
+
+function hidePopup() {
+    clearInterval(this.interval);
+    for (num = 0; num < popups.length; num++) document.getElementById(popups[num] + "Holder").style.display = "none";
+    document.getElementById("popupHolder").style.display = "none";
+}
+
+class krunkerMod {
+    constructor() {
+        this.fileArr = []
+    }
+
+    clear() {
+        this.fileArr.length = 0;
+        zipCList.innerHTML = "";
+    }
+
+    add(file, path) {
+        try {
+            krunkerMod.blobUrlToBlob(URL.createObjectURL(file), path).then((blob) => {
+                this.fileArr.push([blob, path]);
+                let tLi = document.createElement("li");
+                tLi.innerHTML = path;
+                zipCList.appendChild(tLi);
+            })
+        } catch (err) {
+            alert("Please Select a File!")
+        }
+    }
+
+    download() {
+        model.setCreationMethod("Blob");
+        model.addFiles(this.fileArr,
+            () => {},
+            () => {},
+            () => {},
+            () => {
+                model.getBlobURL(function (url) {
+                    krunkerMod.downloadFile(url)
+                });
+            });
+    }
+
+    static blobUrlToBlob(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url).then(r => resolve(r.blob()));
+        })
+    }
+
+    static downloadFile(url) {
+        let dAnchor = document.createElement("a");
+        dAnchor.href = url;
+        dAnchor.download = "Test_Krunker_Mod.zip";
+        dAnchor.style.display = "none";
+        document.body.appendChild(dAnchor);
+        dAnchor.click();
+        dAnchor.remove();
+    }
+}
+
+var mainZip = new krunkerMod();
+
+var obj = this;
+var model = (function () {
+    var zipFileEntry, zipWriter, writer, creationMethod, URL = obj.webkitURL || obj.mozURL || obj.URL;
+
+    return {
+        setCreationMethod: function (method) {
+            creationMethod = method;
+        },
+        addFiles: function addFiles(fileArr, oninit, onadd, onprogress, onend) {
+            var addIndex = 0;
+
+            function nextFile() {
+                var file = fileArr[addIndex]
+                onadd(file);
+
+                zipWriter.add(file[1], new zip.BlobReader(file[0]), function () {
+                    addIndex++;
+                    if (addIndex < fileArr.length)
+                        nextFile();
+                    else
+                        onend();
+                }, onprogress);
+            }
+
+            function createZipWriter() {
+                zip.createWriter(writer, function (writer) {
+                    zipWriter = writer;
+                    oninit();
+                    nextFile();
+                }, onerror);
+            }
+
+            if (zipWriter)
+                nextFile();
+            else if (creationMethod == "Blob") {
+                writer = new zip.BlobWriter();
+                createZipWriter();
+            } else {
+                createTempFile(function (fileEntry) {
+                    zipFileEntry = fileEntry;
+                    writer = new zip.FileWriter(zipFileEntry);
+                    createZipWriter();
+                });
+            }
+        },
+        getBlobURL: function (callback) {
+            zipWriter.close(function (blob) {
+                var blobURL = creationMethod == "Blob" ? URL.createObjectURL(blob) : zipFileEntry.toURL();
+                callback(blobURL);
+                zipWriter = null;
+            });
+        },
+        getBlob: function (callback) {
+            zipWriter.close(callback);
+        }
+    };
+})();
---
+++ js/krunker.js
@@ -0,0 +1,62 @@
+getSkinPath = (a, end = "") => {
+    if (a.pat !== undefined) {
+        return "textures/weapons/pat/" + a.pat + end + ".png";;
+    } else if (a.type === 1 || a.type === 2) {
+        return "textures/" + types[a.type] + a.id + (!a.tex ? "" : `_${a.tex}`) + end + ".png";
+    } else if (a.def === true) {
+        return "textures/" + types[0] + a.weapon + end + ".png";
+    } else {
+        //Krunker's JS/Code (I did not write this code)
+        return "textures/" + (a.type && 4 == a.type ? "sprays/" + a.id : /*"previews/"*/ "" + (a.type && (3 > a.type || 4 < a.type) ? "cosmetics/" + a.type + "_" + a.id + (a.tex ? "_" + a.tex : "") : types[a.type || 0] + (a.type && 3 == a.type ? a.id + (null == a.pat ? null == a.tex ? "" : "_" + a.tex : "_c" + a.pat) : (a.weapon || 0) + "_" + (null == a.mid ? null == a.pat ? a.tex ? a.tex : a.id : "c" + a.pat : "m" + a.mid + (null == a.midT ? "" : "_" + a.midT))))) + end + ".png";
+    }
+}
+
+getSkinPreview = (a) => {
+    if (a.def !== true) {
+        //Krunker's JS/Code (I did not write this code)
+        return "https://assets.krunker.io/textures/" + (a.type && 4 == a.type ? "sprays/" + a.id : "previews/" + (a.type && (3 > a.type || 4 < a.type) ? "cosmetics/" + a.type + "_" + a.id + (a.tex ? "_" + a.tex : "") : types[a.type || 0] + (a.type && 3 == a.type ? a.id + (null == a.pat ? null == a.tex ? "" : "_" + a.tex : "_c" + a.pat) : (a.weapon || 0) + "_" + (null == a.mid ? null == a.pat ? a.tex ? a.tex : a.id : "c" + a.pat : "m" + a.mid + (null == a.midT ? "" : "_" + a.midT))))) + ".png";
+    } else {
+        return "https://assets.krunker.io/textures/previews/" + types[0] + a.weapon + ".png";
+    }
+}
+
+var skinsDiv = document.getElementById("pSkins"),
+    glowSelect = document.getElementById("glow");
+
+updatePF = () => {
+    setTimeout(() => {
+        let sNum = 0;
+        skinsDiv.innerHTML = "";
+        var skinName = pInput.value;
+        for (i of skins) {
+            if (i.name.toLowerCase().includes(skinName.toLowerCase())) {
+                //if (glowSelect.value === "all" || (glowSelect.value = "noglow" && !i.glow) || (glowSelect.value === "glow" && i.glow)) {
+                    let emissive = "";
+                    if (i.glow) {
+                        emissive = `<div class="cardActionSep"></div><a class="cardAction" onclick="input.value = '${getSkinPath(i, "_e")}' /* add emissive */">Add Emissive</a>`
+                    }
+                    let iCard = document.createElement("div"),
+                        iCol = rarities[i.rarity].color;
+                    skinsDiv.appendChild(iCard);
+                    iCard.outerHTML = `
+                <div class="itemCard" style="color:${iCol};border:3px solid ${iCol}">${i.name}<img draggable="false" class="marketImg"
+                src="${getSkinPreview(i)}">
+            <div class="cardActions">
+            <h1 class="cardActionH">${i.name}</h1>
+                <a class="cardAction" onclick="input.value = '${getSkinPath(i)}' /* add normal */">Add Base</a>
+                ${emissive}
+            </div>
+        </div>
+                `
+                    sNum++;
+                //}
+            }
+            //if (sNum >= 9) break;
+        }
+        if (skinsDiv.innerHTML === "") {
+            skinsDiv.innerHTML = "<h1 style=\"font-size: 35px; text-align:center\">No skins found</h1>"
+        }
+    }, 1);
+}
+
+updatePF();
